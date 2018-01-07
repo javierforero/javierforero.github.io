fd5e92390a399215a8cdbe686b83421cf2130ed6
Javier Forero <javierforero01@gmail.com>
2018-01-07 16:54:53 -0600

    Initial _site subtree commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var scrollFunc = function() {
+      $('.home-page__nav li a').click(function(e){
+            var elId = e.target.id;
+            if(elId == 'hp-nav-about') {
+              $('html, body').animate({
+                  scrollTop: $('#about-me').offset().top
+                }, 400);
+            } else if(elId == 'hp-nav-work') {
+              $('html, body').animate({
+                  scrollTop: $('#projects').offset().top
+                }, 400);
+            }
+          });
+};
+
+$(function(){
+  var img = new Image();
+  img.src = '/images/javi_portrait_2.jpg';
+  img.onload = function() {
+    $('body').toggleClass('loaded');
+  };
+});
